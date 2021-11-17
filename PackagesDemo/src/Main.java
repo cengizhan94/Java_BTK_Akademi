f57e09cfa5206e906369344c65dc55c5a43af1cc
Cengiz Han <90911173+cengizhan94@users.noreply.github.com>
2021-11-17 16:36:39 +0300

    Ders17.1-17.2
    Packages

---
+++ PackagesDemo/src/Main.java
@@ -0,0 +1,13 @@
+
+import matematik.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        DortIslem dortIslem = new DortIslem();
+        dortIslem.topla(3,5);
+        System.out.println(dortIslem);
+        Logaritma logaritma = new Logaritma();
+        logaritma.logaritmaHesapla();
+    }
+}
